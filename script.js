c87815f1f3483e691e21703080785559b79dd17a
afcbrandon <128235286+afcbrandon@users.noreply.github.com>
2025-02-09 17:58:26 -0600

    Fixed grammar in alert function from script.js

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
 var guest = prompt("Please enter your name.");
-alert("Welcome, " + guest + " to Brandon's Home Page");
+alert("Welcome, " + guest + ", to Brandon's Home Page");
 document.write("<h2 style='color:blue'>Welcome to Brandon's home page, " + guest + '!<h2>')
